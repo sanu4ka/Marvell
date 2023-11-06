914dca596bcd31d118adf0ef56ad2a867c6e38d6
sanu4ka <sanu4ka1989@gmail.com>
2023-11-06 03:20:50 +0200

    pass

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,8 +1,18 @@
+import md5 from 'md5';
+
 export const fetchData = async searchTerm => {
   const term = searchTerm;
   try {
+    const now = Date.now();
+    const publicKey = 'eed8fd1a9a4254697c0f7daeda206088';
+    const privateKey = 'aefb1aa9df17d0d419e086f920e8fd4ef79c41eb';
+    let params = {
+      apikey: publicKey,
+      ts: now,
+    };
+    params.hash = md5(params.ts + privateKey + publicKey);
     const res = await fetch(
-      `https://gateway.marvel.com/v1/public/${term}?apikey=eed8fd1a9a4254697c0f7daeda206088`
+      `https://gateway.marvel.com/v1/public/${term}?apikey=${params.apikey}&ts=${params.ts}&hash=${params.hash}`
     );
     const data = await res.json();
     localStorage.setItem(term, JSON.stringify(data));
